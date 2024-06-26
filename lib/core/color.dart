f65746883692382ac481bebf5248c453fafafc8d
salembalfaqih <salem.m.it.2020@gmail.com>
2024-06-26 18:49:14 +0300

    add core colors

---
+++ lib/core/color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color lightblack = Color.fromARGB(255, 77, 76, 73);
+
+  static const Color gray = Color.fromARGB(255, 150, 126, 87);
+  static const Color primarycolor =
+      Color.fromARGB(255, 162, 114, 48); //Color.fromARGB(255, 238, 169, 48);
+  static const Color secncolor = Color.fromARGB(255, 255, 255, 255);
+  static const Color black = Color.fromARGB(255, 0, 0, 0);
+  static const Color ambers = Color.fromARGB(255, 239, 164, 51);
+  static const Color rmady = Color.fromARGB(255, 240, 242, 242);
+}
